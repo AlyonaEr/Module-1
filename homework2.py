b3eacc8e10bc455ef26e97760f1ce6598575048d
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-07-11 18:58:05 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+total_talks = 12
+hours_spent = 1.5
+course_name = "Python,"
+time_for_1_task = hours_spent / total_talks
+print("Курс: ",course_name,"всего задач:",total_talks,", затрачено часов:",hours_spent,", среднее время выполнения:",time_for_1_task,"часа")
+print("Курс: "+course_name+" всего задач: "+str(total_talks)+", затрачено часов: "+str(hours_spent)+", среднее время выполнения: "+str(time_for_1_task)+" часа")
+
+
