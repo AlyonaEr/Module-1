d98e6cc41939ffc9e786235eaaf385df73fe5adf
AlyonaEr <A.R.Erschowa@yandex.ru>
2024-07-14 13:52:55 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Организация программ и методы строк ', )
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))     # не срабатывает, выдает ошибку
+print(my_string[0])
+print(my_string[-1])
